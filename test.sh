da3ebdd03998b8efc3d630322cfa709ea5a1b4d6
Simon Walker <s.r.walker101@googlemail.com>
2014-08-30 15:15:00 +0100

    Testing stitching another file

--- test.sh
+++ test.sh
@@ -10,12 +10,13 @@ ensure_link() {
 
 setup_test_files() {
     ensure_link test1.fits /ngts/pipedev/ParanalOutput/03-wasp103b.fits
+    ensure_link test3.fits /ngts/pipedev/ParanalOutput/05-wasp103b.fits
     ensure_link test2.fits /ngts/pipedev/ParanalOutput/08-wasp103b.fits
 }
 
 run_script() {
     test -f output.fits && rm output.fits
-    python ./zlp-stitch.py test1.fits test2.fits -o output.fits
+    python ./zlp-stitch.py test1.fits test2.fits test3.fits -o output.fits
 }
 
 main() {
