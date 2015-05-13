d0be38d7acf8b9dbcc971d2718e75e29566c4b73
Simon Walker <s.r.walker101@googlemail.com>
2015-05-13 09:40:01 +0100

    Getting available paths

--- stitch_all.py
+++ stitch_all.py
@@ -5,16 +5,28 @@ from __future__ import division, print_function, absolute_import
 import subprocess as sp
 import argparse
 from commonlog import setup_logging
+import os
+import re
 
 logger = setup_logging('stitch-all')
 
+regex = re.compile(r'2015\d{4}-(?P<field>.+)-(?P<camera>80\d)$')
 
-def valid(x):
-    return x % 2 == 0
+CHOSEN_FIELDS = {'wasp19b', 'k2_3b', 'ng2000'}
+CHOSEN_CAMERAS = {802, 805, 806}
+
+
+def valid(path):
+    return regex.search(path)
 
 
 def get_all_field_cameras():
-    return xrange(10)
+    data_dir = os.path.join('/', 'ngts', 'pipedev', 'ParanalOutput',
+                            'running-the-pipeline')
+    cmd = map(str, ['find', data_dir, '-maxdepth', 1, '-type', 'd'])
+    logger.debug('cmd: %s', cmd)
+    for line in sp.check_output(cmd).split():
+        yield line.strip()
 
 
 def get_available_field_cameras():
@@ -22,10 +34,13 @@ def get_available_field_cameras():
 
 
 def main(args):
-    for field_camera_combo in get_available_field_cameras():
-        logger.info('Building {}'.format(field_camera_combo))
+    if args.verbose:
+        logger.setLevel('DEBUG')
+    for path in get_available_field_cameras():
+        logger.info('Building {}'.format(path))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
     main(parser.parse_args())
