eee6ab00dc7df115f26af684945cd594f73b92e3
Simon Walker <s.r.walker101@googlemail.com>
2015-05-21 11:03:37 +0100

    Add logging

--- scripts/resort_by_mjd.py
+++ scripts/resort_by_mjd.py
@@ -14,12 +14,15 @@ IGNORE_HDUS = {'PRIMARY', 'CASUDET'}
 
 
 def resort(filename):
+    logger.info('Sorting %s', filename)
     with fits.open(filename) as infile:
         imagelist = infile['imagelist'].data
         tmid = imagelist['tmid']
         ind = np.argsort(tmid)
 
+    logger.debug('Opening file for updating')
     with fits.open(filename, mode='update') as infile:
+        logger.info(' Sorting imagelist')
         infile['imagelist'].data[:] = imagelist[ind]
 
         images = (hdu for hdu in infile if hdu.is_image)
@@ -27,6 +30,7 @@ def resort(filename):
                 if image.name.upper() not in IGNORE_HDUS)
 
         for image in valid_images:
+            logger.info(' Sorting %s', image.name)
             name = image.name
             sorted_data = image.data[:, ind]
             infile[name].data = sorted_data[:]
