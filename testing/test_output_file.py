bd4e7c16dbacf0f5600ed1ac3f0d33e524e5c3fd
Simon Walker <s.r.walker101@googlemail.com>
2015-05-21 11:07:58 +0100

    Skip output file test if test file does not exist

--- testing/test_output_file.py
+++ testing/test_output_file.py
@@ -1,15 +1,15 @@
 import numpy as np
 from astropy.io import fits
 import pytest
+import os
+
+TEST_FILENAME = 'out.fits'
 
 
-@pytest.fixture
-def resultsfile():
-    return 'out.fits'
-
-
-def test_sorted_mjd_order(resultsfile):
-    imagelist = fits.getdata(resultsfile, 'imagelist')
+@pytest.mark.skipif(not os.path.isfile(TEST_FILENAME),
+        reason="cannot find test source file")
+def test_sorted_mjd_order():
+    imagelist = fits.getdata(TEST_FILENAME, 'imagelist')
     tmid = imagelist['tmid']
 
     assert (tmid == np.sort(tmid)).all()
